b13b35a20d6742489bde58960d6999e7471632cd
matej-kotrba <matekotrba.skola@gmail.com>
2023-04-16 14:46:58 +0200

    Setuping authjs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,16 @@
+import { sequence } from "@sveltejs/kit/hooks";
 import { createContext } from "./lib/trpc/context";
 import { router } from "./lib/trpc/router"
 import type { Handle } from "@sveltejs/kit";
 import { createTRPCHandle } from "trpc-sveltekit"
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from "@auth/core/providers/github"
 
-export const handle: Handle = createTRPCHandle({
+const handleTRPCContext: Handle = createTRPCHandle({
   router: router,
   createContext: createContext,
 })
+
+const handleAuth: Handle = SvelteKitAuth
+
+export const handle = sequence(handleTRPCContext)
