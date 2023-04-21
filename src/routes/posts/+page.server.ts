97cd54fc3aaec3c508e409f38c4821416d912749
mkit2009 <matejkotrba.skola@gmail.com>
2023-04-21 20:49:47 +0200

    Comment

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -2,6 +2,7 @@ import type { ServerLoad } from "@sveltejs/kit";
 import prisma from "$lib/prisma";
 
 export const load: ServerLoad = async ({ params }) => {
+  // Getting posts from MongoDB
   const posts = await prisma.post.findMany({
     select: {
       title: true,
