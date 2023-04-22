af6f99a284e717e98b62a69d5f94d3649aa5d937
mkit2009 <matejkotrba.skola@gmail.com>
2023-04-22 23:56:09 +0200

    Commit added

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -2,6 +2,7 @@ import prisma from "../prisma"
 import type { RequestEvent } from "@sveltejs/kit";
 import type { inferAsyncReturnType } from "@trpc/server";
 
+// Creates trpc context
 export async function createContext(event: RequestEvent) {
   return {
     prisma: prisma
